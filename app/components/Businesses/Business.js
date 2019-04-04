2ff5057731b6a14990798e696673c0f48f9a4988
Iharidla <Ihar.Pikaliuk@itechart-group.com>
2019-04-04 17:30:10 +0300

    styles: remove business styles to styles.js

--- app/components/Businesses/Business.js
+++ app/components/Businesses/Business.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import {StyleSheet, Text, View, Image, Linking, TouchableOpacity} from 'react-native';
+import {Text, View, Image, Linking, TouchableOpacity} from 'react-native';
+
+import styles from "./styles";
 
 const Business = ({name, url, image_url, categories, address, phone,  price, rating}) => {
   
   return(
     <TouchableOpacity style={styles.resultRow} onPress={() => Linking.openURL(url)}>
-      <View style={styles.image}>
+      <View style={styles.imageContainer}>
         <Image source={{uri: image_url}}
-               style={{width: 80, height: 80}}/>
+               style={styles.image}/>
       </View>
       <View style={styles.description}>
-        <Text style={{fontWeight: 'bold'}}>{`${name}`}</Text>
-        <Text style={{flexDirection: 'row'}}>
+        <Text style={styles.nameText}>{`${name}`}</Text>
+        <Text style={styles.categoryText}>
           {categories.map((item) => (item.title))}
         </Text>
         <Text>{`${address}`}</Text>
@@ -25,44 +27,5 @@ const Business = ({name, url, image_url, categories, address, phone,  price, rat
     </TouchableOpacity>
   );
 };
-
-const styles = StyleSheet.create({
-  resultRow: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'flex-start',
-    margin: 10,
-  },
-
-  price: {
-    position: 'absolute',
-    bottom: 25,
-  },
-
-  rating: {
-    position: 'absolute',
-    top: 5,
-  },
-  
-  image:{
-    justifyContent: 'flex-start',
-    flexBasis: '25%',
-  },
-  
-  description:{
-    flexDirection: 'column',
-    justifyContent: 'center',
-    flexBasis: '65%',
-    textAlign: 'left',
-  },
-  
-  info: {
-    flexDirection: 'column',
-    justifyContent: 'flex-end',
-    flexBasis: '10%',
-    textAlign: 'center',
-  },
-  
-});
 
 export default Business;
